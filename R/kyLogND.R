8fc3329ea4ea896d0d9dd78b203bdaae35f88709
山路憲 <ken.yamaji@gmail.com>
2022-05-04 11:41:39 +0900

    20220504

--- R/kyLogND.R
+++ R/kyLogND.R
@@ -1,5 +1,5 @@
-#' @title Generate random numbers with log normal distribution
-#' @description \code{rlnorm2} Generate random numbers with log normal distribution
+#' @title Generate random numbers in log normal distribution
+#' @description \code{rlnorm2} generates random numbers in log normal distribution
 #'
 #' @param n Length of generated list
 #' @param mean Mean of original distribution
@@ -11,3 +11,50 @@ rlnorm2 <- function(n, mean = 1, sd = 1) {
   meanlog <- log(mean) - (sdlog^2) / 2
   rlnorm(n, meanlog = meanlog, sdlog = sdlog)
 }
+
+
+#' @title Convert log converted mean to the original mean
+#' @description \code{convertLogToMean} converts log converted mean to the original mean
+#'
+#' @param meanlog Mean of log converted distribution
+#' @param sdlog Standard deviation of log converted distribution
+#' @return Mean of original distribution
+#' @export
+convertLogToMean <- function(meanlog = 1, sdlog = 1) {
+  exp(meanlog + (sdlog^2) / 2)
+}
+
+
+#' @title Convert log converted SD to the original SD
+#' @description \code{convertLogToSD} converts log converted SD to the original SD
+#'
+#' @param meanlog Mean of log converted distribution
+#' @param sdlog Standard deviation of log converted distribution
+#' @return SD of original distribution
+#' @export
+convertLogToSD <- function(meanlog = 1, sdlog = 1) {
+  exp(2*meanlog + (sdlog^2)) * (exp(sdlog^2) - 1)
+}
+
+
+#' @title Compare histogram between original and log converted distribution
+#' @description \code{histLogND} draw histogram of original and log converted distribution.
+#' Red vertical line shows the mean of the original distribution.
+#' Blue vertical line shows the mean of log converted distribution.
+#'
+#' @param data A list of log normal distribution]
+#' @export
+histLogND <- function(data) {
+  par(mfrow = c(1, 2))
+  data.log <- log(data)
+
+  hist(data)
+  abline(v=mean(data), col="red")
+  abline(v=exp(mean(data.log)), col="blue")
+
+
+  hist(data.log)
+  abline(v=log(mean(data)), col="red")
+  abline(v=mean(data.log), col="blue")
+}
+
--- README.md
+++ README.md
@@ -7,53 +7,26 @@
 
 <!-- badges: end -->
 
-The goal of kyLogND is to …
+kyLogND is a set of utility functions to easily manipulate log normal
+distribution.
 
 ## Installation
 
-You can install the released version of kyLogND from
-[CRAN](https://CRAN.R-project.org) with:
-
-``` r
-install.packages("kyLogND")
-```
-
-And the development version from [GitHub](https://github.com/) with:
+You can install the development version from
+[GitHub](https://github.com/) with:
 
 ``` r
 # install.packages("devtools")
 devtools::install_github("kenyam1979/kyLogND")
 ```
 
-## Example
-
-This is a basic example which shows you how to solve a common problem:
+## Utility functions in kyLogND
 
 ``` r
-# library(kyLogND)
-## basic example code
+library(kyLogND)
+
+logND <- rlnorm2(100)
+hist(logND)
 ```
 
-What is special about using `README.Rmd` instead of just `README.md`?
-You can include R chunks like so:
-
-``` r
-summary(cars)
-#>      speed           dist       
-#>  Min.   : 4.0   Min.   :  2.00  
-#>  1st Qu.:12.0   1st Qu.: 26.00  
-#>  Median :15.0   Median : 36.00  
-#>  Mean   :15.4   Mean   : 42.98  
-#>  3rd Qu.:19.0   3rd Qu.: 56.00  
-#>  Max.   :25.0   Max.   :120.00
-```
-
-You’ll still need to render `README.Rmd` regularly, to keep `README.md`
-up-to-date.
-
-You can also embed plots, for example:
-
-<img src="man/figures/README-pressure-1.png" width="100%" />
-
-In that case, don’t forget to commit and push the resulting figure
-files, so they display on GitHub\!
+<img src="man/figures/README-example-1.png" width="100%" />
